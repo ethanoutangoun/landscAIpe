3ee0c6762693b34b4c8b21c6af71a017e7f0a7f3
ethanoutangoun <ethanoutangoun@gmail.com>
2023-10-31 18:51:48 -0700

    barebones navbar

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-col">
+        <img src="favicon.ico" alt="nav-icon" width="30px" />
+        <h1>LandScAIpe</h1>
+      </div>
+      <div className="navbar-colw">
+        
+      </div>
+      <div className="navbar-col">
+        <p>Sign In</p>
+        <p>Sign Up</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
