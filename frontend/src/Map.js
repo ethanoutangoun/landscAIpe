d62ed1a838db0c25d2240ab16f0bd8a4f8583793
ethanoutangoun <ethanoutangoun@gmail.com>
2023-11-09 20:16:08 -0800

    hide pub key

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,13 +1,41 @@
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
 const Map = () => {
-  return (
-    <div className="map-container">
-      <h1>Map</h1>
-      
-      
-    
-    </div>
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-120.6556);
+  const [lat, setLat] = useState(35.2901);
+  const [zoom, setZoom] = useState(12);
 
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div className="map-container-main">
+      <h1>Map</h1>
+      <div className="map-container-wrapper">
+        <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
+    </div>
   );
 };
 
