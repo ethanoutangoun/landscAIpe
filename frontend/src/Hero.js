bacf43746dc521c2c8434d58c379cc25fc863f2a
ethanoutangoun <ethanoutangoun@gmail.com>
2023-11-13 02:52:43 -0800

    gradient icon

--- frontend/src/Hero.js
+++ frontend/src/Hero.js
@@ -7,7 +7,13 @@ const Hero = () => {
   return (
     <div className="hero">
       <h1>Plan Smarter with</h1>
-      <h3>LandScAIpe</h3>
+      <div className="title-sec">
+        <h3>LandSc</h3>
+        <h3 className="gradient-ai">AI</h3> 
+        
+        <h3>pe</h3>
+      </div>
+      
       <div className="hero-descriptor">
         <p>
           LandScAIpe is a web application that allows users to plan their
